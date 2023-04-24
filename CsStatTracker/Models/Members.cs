d2f71f2accb14488da71c56219bd9ac095b83c37
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-04-23 21:16:21 -0700

    Added the LoginViewModel and RegisterViewModel into the members class for neat organization.

--- CsStatTracker/Models/Members.cs
+++ CsStatTracker/Models/Members.cs
@@ -26,4 +26,64 @@ namespace CsStatTracker.Models
         [Key]
         public int MemberID { get; set; }
     }
+    
+    /// <summary>
+    /// ViewModel To allow a register page to be linked with the members controller.
+    /// </summary>
+    public class RegisterViewModel
+    {
+        /// <summary>
+        /// Email is required and the maximum characters can be up to 100.
+        /// The only current validation is that the email must require an @ sign.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+        /// <summary>
+        /// ConfirmEmail allows for the 2nd box on the registration page to match
+        /// the initial email registered with to confirm that the email provided is correct.
+        /// </summary>
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+        /// <summary>
+        /// Password is required for the user to register and must be at least 6 characters
+        /// but no more than 75. The password will remain hidden until the user presses the show
+        /// password button that is usually provided by the browser.
+        /// </summary>
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        /// <summary>
+        /// Works the same as ConfirmEmail and allows the user to confirm their password before
+        /// sending it to the database.
+        /// </summary>
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+    /// <summary>
+    /// ViewModel to allow a login page to be linked with the members controller.
+    /// </summary>
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// The required email address that the user registered with on the registration page.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// The required password that the user registered with on the register page.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
 }
